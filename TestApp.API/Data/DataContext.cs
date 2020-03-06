bd1c8684f6df4395f85e26ea055d25a596ee15f1
Sean Patrick C. Daga <SCDaga@goldenhaven.com.ph>
2020-03-06 15:23:41 +0800

    Initial Commit

---
+++ TestApp.API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using TestApp.API.Models;
+
+namespace TestApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options)
+        {
+            
+        }
+        public DbSet<Value> Values { get; set; }
+    }
+}
